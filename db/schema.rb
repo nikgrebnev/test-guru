5c1000b507ad44b2d181affb01becce4f0afa418
Nik Grebnev <nikolaygrebnev@gmail.com>
2019-10-22 22:44:39 +0300

    lesson3, fix3

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2019_10_22_185304) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
-    t.boolean "correct"
-    t.integer "user_id", null: false
-    t.integer "question_id", null: false
+    t.boolean "correct", default: false, null: false
+    t.bigint "user_id", null: false
+    t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
@@ -30,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_10_22_185304) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
-    t.integer "test_id", null: false
+    t.bigint "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 2019_10_22_185304) do
   create_table "tests", force: :cascade do |t|
     t.text "title", null: false
     t.integer "level", default: 0, null: false
-    t.integer "category_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
