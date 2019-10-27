4113cf0e9076ea85851529cf7345f6f3db8cc5fb
Nik Grebnev <nikolaygrebnev@gmail.com>
2019-10-27 15:29:46 +0300

    lesson5 in (lesson4 granch)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :passes
+  has_many :tests, through: :passes
 
   def passed_tests(level)
     Test.where("level = ? and id in (select test_id from passes where user_id = ?)", level.to_i, self.id).select(:id, :title)
