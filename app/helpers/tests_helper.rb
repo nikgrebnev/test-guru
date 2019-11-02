7f0a6c31855f45f5a8c25f84d839c95dbe608f48
Nik Grebnev <nikolaygrebnev@gmail.com>
2019-11-02 17:30:45 +0300

    lesson 8, f2

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,7 @@
 module TestsHelper
+
+  TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }
+  def test_level(test)
+    "#{test.level}-#{TEST_LEVELS[test.level] || :hero}"
+  end
 end
