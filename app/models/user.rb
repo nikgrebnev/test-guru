5b6b2b6d91533097fa540d28edd6f83f723caf5e
Nik Grebnev <nikolaygrebnev@gmail.com>
2019-11-22 23:28:10 +0300

    lesson 11, fix3

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: "Test", foreign_key: :author_id
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def passed_tests(level)
     tests.by_level(level).select(:id, :title)
