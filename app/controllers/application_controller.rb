4b00b8a6ac50b9ca4b131dd8381297cbf7ecd9b7
Nik Grebnev <nikolaygrebnev@gmail.com>
2019-11-26 21:01:54 +0300

    lesson 12, fix1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,12 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def default_url_options
-    { lang: current_locale }
+    I18n.locale == I18n.default_locale ? {} : { lang: I18n.locale }
   end
 
   private
 
   def current_locale
-    I18n.locale == I18n.default_locale ? nil : I18n.locale
   end
 
   def set_locale
