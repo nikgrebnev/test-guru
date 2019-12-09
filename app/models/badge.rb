239e21c47be3ec653002f0c7645ef34bf8be5b51
Nik Grebnev <nikolaygrebnev@gmail.com>
2019-12-09 21:33:49 +0300

    lesson18-badges, fix 2

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,4 +3,6 @@ class Badge < ApplicationRecord
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
+  validates :title, :url, :badge_rule, :badge_type, presence: true
+  
 end
